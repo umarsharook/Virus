468a23a754ebc0988401aeba6a4f90005af21851
Umar Sharook <139246587+umarsharook@users.noreply.github.com>
2025-04-13 13:04:02 +0530

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,8 @@
+import webbrowser
+import time
+
+# Wait for 10 seconds or change this to 1-2 seconds for destroy the device
+time.sleep(0.5)
+
+# Open YouTube
+webbrowser.open("https://www.youtube.com")
